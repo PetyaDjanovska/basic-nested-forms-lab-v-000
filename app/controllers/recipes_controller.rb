8f1c868ec40658fad7c1da0fd8c5f4f1f7d743d8
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-11 17:02:52 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,7 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
     @recipe.ingredients.build(title: 'cake')
+    @recipe.ingredients.build(title: 'omlette')
   end
 
   def create
